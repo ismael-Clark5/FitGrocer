eaa71492fd957c212f84cfb154d4445b6880e655
ismael-Clark <iclar013@fiu.edu>
2024-04-14 14:26:27 -0400

    First commit into dev

---
+++ FitGrocerBackend/fitgrocer/src/main/java/fitgrocer/com/fitgrocer/FitgrocerApplication.java
@@ -0,0 +1,13 @@
+package fitgrocer.com.fitgrocer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitgrocerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitgrocerApplication.class, args);
+	}
+
+}
---
+++ FitGrocerBackend/fitgrocer/src/test/java/fitgrocer/com/fitgrocer/FitgrocerApplicationTests.java
@@ -0,0 +1,13 @@
+package fitgrocer.com.fitgrocer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitgrocerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
